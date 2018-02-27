a92f3a08a9fbe34e711212d489c1b2ba143cd085
sunny <16xyd1@queensu.ca>
2018-02-27 13:10:03 -0500

    Created a button linking Main and MainMenu

--- app/src/main/java/com/example/sunny/freelancenow/MainActivity.java
+++ app/src/main/java/com/example/sunny/freelancenow/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.sunny.freelancenow;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button1 = (Button)findViewById(R.id.button1);
+        button1.setOnClickListener(this);
+
+    }
+
+    public void onClick (View view)
+    {
+        Intent i;
+        i = new Intent(this, MainMenuActivity.class);
+        startActivity(i);
     }
 }
