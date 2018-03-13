2e0dfcb6d492924aabb3e8b01f1a99290697966e
sunny <16xyd1@queensu.ca>
2018-03-13 04:50:26 -0400

    Created UI for category actvity and cleaned up some of main menu activity

--- app/src/main/java/com/example/sunny/freelancenow/MainActivity.java
+++ app/src/main/java/com/example/sunny/freelancenow/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.sunny.freelancenow;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,7 +12,7 @@ public class MainActivity extends Activity implements View.OnClickListener{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button button1 = (Button)findViewById(R.id.button1);
+        Button button1 = (Button)findViewById(R.id.buttonc1);
         button1.setOnClickListener(this);
         Button button2 = (Button)findViewById(R.id.button2);
         button2.setOnClickListener(this);
