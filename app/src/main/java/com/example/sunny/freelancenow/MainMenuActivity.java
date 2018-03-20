76fd3aa000ea705fde4cf6ee01f890aaaf9dc642
sunny <16xyd1@queensu.ca>
2018-03-19 21:41:47 -0400

    added jsoup html parser

--- app/src/main/java/com/example/sunny/freelancenow/MainMenuActivity.java
+++ app/src/main/java/com/example/sunny/freelancenow/MainMenuActivity.java
@@ -6,8 +6,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
 
 public class MainMenuActivity extends Activity{
+
+    TextView txtView = (TextView)findViewById(R.id.txtView);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +33,34 @@ public class MainMenuActivity extends Activity{
             }
         });
 
+        URL url;
+
+        try {
+            // get URL content
+
+            String a="192.168.56.1";
+            url = new URL(a);
+            URLConnection conn = url.openConnection();
+
+            // open the stream and put it into BufferedReader
+            BufferedReader br = new BufferedReader(
+                    new InputStreamReader(conn.getInputStream()));
+
+            String inputLine;
+            while ((inputLine = br.readLine()) != null) {
+                System.out.println(inputLine);
+                txtView.setText(inputLine);
+            }
+            br.close();
+
+            System.out.println("Done");
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
+
 }
